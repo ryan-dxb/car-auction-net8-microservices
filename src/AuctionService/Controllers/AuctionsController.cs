a130326268b62a1641f61b9ff1d7cf3ee6175c23
Ryan P <93515317+ryan-dxb@users.noreply.github.com>
2023-12-12 15:21:17 +0400

    Completed Complete CRUD for Auctions

--- src/AuctionService/Controllers/AuctionsController.cs
+++ src/AuctionService/Controllers/AuctionsController.cs
@@ -1,5 +1,6 @@
 ﻿using AuctionService.Data;
 using AuctionService.DTOs;
+using AuctionService.Entities;
 using AutoMapper;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -44,6 +45,83 @@ namespace AuctionService.Controllers
             }
 
             return mapper.Map<AuctionDTO>(auction);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<AuctionDTO>> CreateAuction([FromBody] CreateAuctionDTO createAuctionDTO)
+        {
+            var auction = mapper.Map<Auction>(createAuctionDTO);
+
+            // TODO:Add Current User as seller
+            auction.Seller = "Test";
+
+            auctionDbContext.Auctions.Add(auction);
+
+
+
+            var result = await auctionDbContext.SaveChangesAsync() > 0;
+            if (!result)
+            {
+                return BadRequest();
+            }
+
+
+            return CreatedAtAction(nameof(GetAuctionById), new { id = auction.Id }, mapper.Map<AuctionDTO>(auction));
+        }
+
+        [HttpPut]
+        [Route("{id}")]
+        public async Task<ActionResult> UpdateAuction([FromRoute] Guid id, [FromBody] UpdateAuctionDTO updateAuctionDTO)
+        {
+            Console.WriteLine(id);
+
+            var auction = await auctionDbContext.Auctions.Include(x => x.Item).FirstOrDefaultAsync(x => x.Id == id);
+
+            if (auction == null)
+            {
+                return NotFound();
+            }
+
+
+            // TODO: Check seller is current user
+            auction.Item.Make = updateAuctionDTO.Make ?? auction.Item.Make;
+            auction.Item.Model = updateAuctionDTO.Model ?? auction.Item.Model;
+            auction.Item.Year = updateAuctionDTO.Year ?? auction.Item.Year;
+            auction.Item.Color = updateAuctionDTO.Color ?? auction.Item.Color;
+            auction.Item.Mileage = updateAuctionDTO.Mileage ?? auction.Item.Mileage;
+
+
+            var result = await auctionDbContext.SaveChangesAsync() > 0;
+            if (!result)
+            {
+                return BadRequest();
+            }
+
+            return NoContent();
+        }
+
+        [HttpDelete]
+        [Route("{id}")]
+        public async Task<ActionResult> DeleteAuction([FromRoute] Guid id)
+        {
+            var auction = await auctionDbContext.Auctions.FirstOrDefaultAsync(x => x.Id == id);
+
+            if (auction == null)
+            {
+                return NotFound();
+            }
+
+            // TODO: check seller is current user
+
+            auctionDbContext.Remove(auction);
+
+            var result = await auctionDbContext.SaveChangesAsync() > 0;
+            if (!result)
+            {
+                return BadRequest();
+            }
+
+            return NoContent();
         }
     }
 }
--- src/AuctionService/DTOs/UpdateAuctionDTO.cs
+++ src/AuctionService/DTOs/UpdateAuctionDTO.cs
@@ -2,14 +2,14 @@
 {
     public class UpdateAuctionDTO
     {
-        public string Make { get; set; }
+        public string? Make { get; set; }
 
-        public string Model { get; set; }
+        public string? Model { get; set; }
 
-        public int Year { get; set; }
+        public int? Year { get; set; }
 
-        public string Color { get; set; }
+        public string? Color { get; set; }
 
-        public int Mileage { get; set; }
+        public int? Mileage { get; set; }
     }
 }
