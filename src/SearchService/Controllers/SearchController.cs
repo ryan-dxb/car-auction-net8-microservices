14c71230ddbc88dc9cceef27a31e0867cce23eb7
Ryan P <93515317+ryan-dxb@users.noreply.github.com>
2023-12-12 17:43:26 +0400

    Added paged search with multiple flitering and sorting options

--- src/SearchService/Controllers/SearchController.cs
+++ src/SearchService/Controllers/SearchController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using MongoDB.Entities;
 using SearchService.Models;
+using SearchService.RequestHelpers;
 
 namespace SearchService.Controllers
 {
@@ -9,20 +10,54 @@ namespace SearchService.Controllers
     public class SearchController : ControllerBase
     {
         [HttpGet]
-        public async Task<ActionResult<List<Item>>> SearchItem([FromQuery] string? q = null)
+        public async Task<ActionResult<List<Item>>> SearchItem(
+            [FromQuery] SearchParams searchParams)
+
         {
-            var query = DB.Find<Item>();
+            var query = DB.PagedSearch<Item, Item>();
 
-            query.Sort(x => x.Ascending(a => a.Make));
 
-            if (!string.IsNullOrEmpty(q))
+            if (!string.IsNullOrEmpty(searchParams.Query))
             {
-                query.Match(Search.Full, q).SortByTextScore();
+                query.Match(Search.Full, searchParams.Query).SortByTextScore();
             }
+
+            query = searchParams.OrderBy switch
+            {
+                "make" => query.Sort(x => x.Ascending(a => a.Make)),
+                "new" => query.Sort(x => x.Descending(a => a.CreatedAt)),
+                _ => query.Sort(x => x.Ascending(a => a.AuctionEndsAt))
+            };
+
+            query = searchParams.FilterBy switch
+            {
+                "finished" => query.Match(x => x.AuctionEndsAt < DateTime.UtcNow),
+                "endingSoon" => query.Match(x => x.AuctionEndsAt < DateTime.UtcNow.AddHours(6)
+                    && x.AuctionEndsAt > DateTime.UtcNow),
+                _ => query.Match(x => x.AuctionEndsAt > DateTime.UtcNow)
+            };
+
+            if (!string.IsNullOrEmpty(searchParams.Seller))
+            {
+                query.Match(x => x.Seller == searchParams.Seller);
+            }
+
+            if (!string.IsNullOrEmpty(searchParams.Winner))
+            {
+                query.Match(x => x.Winner == searchParams.Winner);
+            }
+
+            query.PageNumber(searchParams.PageNumber);
+            query.PageSize(searchParams.PageSize);
 
             var result = await query.ExecuteAsync();
 
-            return Ok(result);
+            return Ok(new
+            {
+                results = result.Results,
+                pageCount = result.PageCount,
+                totalCount = result.TotalCount,
+            });
         }
     }
 }
---
+++ src/SearchService/RequestHelpers/SearchParams.cs
@@ -0,0 +1,21 @@
+﻿namespace SearchService.RequestHelpers
+{
+    public class SearchParams
+    {
+        public string Query { get; set; }
+
+        public int PageSize { get; set; }
+
+        public int PageNumber { get; set; }
+
+        public string Seller { get; set; }
+
+        public string Winner { get; set; }
+
+        public string OrderBy { get; set; }
+
+        public string OrderDirection { get; set; }
+
+        public string FilterBy { get; set; }
+    }
+}
