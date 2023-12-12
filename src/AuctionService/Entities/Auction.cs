747cdf49ea7729081de64a3d811704c53d987c01
Ryan P <93515317+ryan-dxb@users.noreply.github.com>
2023-12-12 11:51:51 +0400

    Add project files.

---
+++ src/AuctionService/Entities/Auction.cs
@@ -0,0 +1,37 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace AuctionService.Entities
+{
+    [Table("Auctions")]
+    public class Auction
+    {
+        public Guid Id { get; set; }
+
+        public int ReservePrice { get; set; } = 0;
+
+        public string Seller { get; set; }
+
+        public string Winner { get; set; }
+
+        public int? SoldAmount { get; set; }
+
+        public int? CurrentHighBid { get; set; }
+
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+
+        public DateTime LastUpdatedAt { get; set; } = DateTime.UtcNow;
+
+        public DateTime AuctionEndsAt { get; set; }
+
+        public Status Status { get; set; }
+
+        // Navigation properties
+        public Item Item { get; set; }
+
+        public Guid ItemId { get; set; }
+
+
+
+
+    }
+}
