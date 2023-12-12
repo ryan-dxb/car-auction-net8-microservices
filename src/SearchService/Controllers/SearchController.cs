4b1f70aef88bcc2a45ab842421ee1bc9689b41a0
Ryan P <93515317+ryan-dxb@users.noreply.github.com>
2023-12-12 16:28:17 +0400

    Added Search Service and added search controller

---
+++ src/SearchService/Controllers/SearchController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MongoDB.Entities;
+using SearchService.Models;
+
+namespace SearchService.Controllers
+{
+    [ApiController]
+    [Route("api/search")]
+    public class SearchController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<ActionResult<List<Item>>> SearchItem([FromQuery] string? q = null)
+        {
+            var query = DB.Find<Item>();
+
+            query.Sort(x => x.Ascending(a => a.Make));
+
+            if (!string.IsNullOrEmpty(q))
+            {
+                query.Match(Search.Full, q).SortByTextScore();
+            }
+
+            var result = await query.ExecuteAsync();
+
+            return Ok(result);
+        }
+    }
+}
